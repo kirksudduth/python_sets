211e1f04b5db14d920aaa565aff49aee271bf49b
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-13 16:01:11 -0500

    working through it

---
+++ cars.py
@@ -0,0 +1,12 @@
+showroom = set()
+
+showroom.add("galaxie")
+showroom.add("element")
+showroom.add("flex")
+showroom.add("el camino")
+showroom.add("el camino")
+
+print(len(showroom))
+print(showroom)
+
+# NUMBER 6
