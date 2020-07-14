e5771480051411f421defdd4fdc55e9ec0eff618
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-13 21:24:45 -0500

    oops been doing everything on master

--- cars.py
+++ cars.py
@@ -6,7 +6,10 @@ showroom.add("flex")
 showroom.add("el camino")
 showroom.add("el camino")
 
+showroom.update(["maxima", "civic"])
+
 print(len(showroom))
+showroom.discard("civic")
 print(showroom)
 
 # NUMBER 6
